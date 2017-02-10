19051adc5d6a67afc7a89ef22ff47aa10e98a505
Javier Evelyn <javier@appvida.co>
2017-02-10 15:29:43 -0500

    ererqw

---
+++ GitBusy/NewViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewViewController.swift
+//  GitBusy
+//
+//  Created by Javier Evelyn on 2/10/17.
+//  Copyright © 2017 iD Tech. All rights reserved.
+//
+
+import Foundation
--- GitBusy/ViewController.swift
+++ GitBusy/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Let's Go!")
+        
     }
 
     override func didReceiveMemoryWarning() {
